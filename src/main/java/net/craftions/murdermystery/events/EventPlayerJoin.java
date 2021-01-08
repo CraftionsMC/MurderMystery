d2d66fab01d37e840c5fb37f623c3f6424d6a09e
MCTzOCK <dev@mctzock.de>
2021-01-08 19:00:25 +0100

    Added ; x2

--- src/main/java/net/craftions/murdermystery/events/EventPlayerJoin.java
+++ src/main/java/net/craftions/murdermystery/events/EventPlayerJoin.java
@@ -18,12 +18,12 @@ public class EventPlayerJoin implements Listener{
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e)
 	{
-		Location l0 = new Location(Bukkit.getWorld("world"), -119, 64, -182)
+		Location l0 = new Location(Bukkit.getWorld("world"), -119, 64, -182);
 		e.getPlayer().teleport(l0);
 		
 		if(PlayerUtil.playerCount() >= Config.minPlayers && PlayerUtil.playerCount() <= Config.maxPlayers && !Murder.isStarted && Config.canAutoStart)
 		{
-			Location l1 = new Location(Bukkit.getWorld("world"), -79, 50, -184)
+			Location l1 = new Location(Bukkit.getWorld("world"), -79, 50, -184);
 			e.getPlayer().teleport(l1);
 			GameUtil.startGame(false);
 			e.getPlayer().setGameMode(GameMode.ADVENTURE);
